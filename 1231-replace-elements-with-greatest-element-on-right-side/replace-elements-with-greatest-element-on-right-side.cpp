a08beb2d75c99d9b998e5d6bb56444ac61067954
shankar791 <84394692+shankar791@users.noreply.github.com>
2026-08-12 21:55:53 +0530

    Time: 3 ms (45.28%) | Memory: 72.9 MB (13.56%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ans(n, -1);
+
+        int maxi = -1;
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            ans[i] = maxi;
+
+            maxi = max(maxi, arr[i]);
+        }
+
+        return ans;
+    }
+};
