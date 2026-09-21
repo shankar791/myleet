882a8a7340136c1027e400c0c8fd44e646559e6f
shankar791 <84394692+shankar791@users.noreply.github.com>
2026-09-21 21:30:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (6.16%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -9,19 +9,15 @@ public:
             auto [left, right] = q.front();
             q.pop();
 
-            // Both are NULL
             if (left == nullptr && right == nullptr)
                 continue;
 
-            // Only one is NULL
             if (left == nullptr || right == nullptr)
                 return false;
 
-            // Values are different
             if (left->val != right->val)
                 return false;
 
-            // Compare mirror children
             q.push({left->left, right->right});
             q.push({left->right, right->left});
         }
